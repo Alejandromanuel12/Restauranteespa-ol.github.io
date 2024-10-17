387fe13bc03c8d53ceb2fdd59fe76907e946aff5
Alejandromanuel12 <alissonculero@gmail.com>
2024-10-16 22:08:58 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Array de productos
+const productos = {
+    comida: [
+        {
+            title: "Gazpacho Andaluz",
+            description: "Un refrescante y saludable plato frío de verduras, ideal para los días calurosos. Hecho con tomates, pepinos, pimientos, aceite de oliva y un toque de vinagre.",
+            price: "$8.00",
+            image: "https://images.unsplash.com/photo-1564544173417-9a86d4cfed09?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&q=80&w=400",
+        },
+        {
+            title: "Tortilla Española",
+            description: "Tradicional tortilla de patatas, con cebolla y huevos, dorada por fuera y suave por dentro.",
+            price: "$7.00",
+            image: "https://images.unsplash.com/photo-1618207118793-d62ff09b5c84?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&q=80&w=400",
+        },
+        {
+            title: "Paella Valenciana",
+            description: "El plato más famoso de España. Hecho con arroz, azafrán, mariscos frescos y verduras, cocinado a la perfección.",
+            price: "$15.00",
+            image: "https://images.unsplash.com/photo-1598799175669-b5c3e65e34ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&q=80&w=400",
+        },
+        {
+            title: "Churros con Chocolate",
+            description: "Deliciosos churros fritos servidos con un espeso chocolate caliente para mojar.",
+            price: "$5.00",
